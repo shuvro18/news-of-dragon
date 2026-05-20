95592ed70e917c3d19059bd2437477da6a331201
shuvro18 <shuvrobiswas404@gmail.com>
2026-05-20 09:30:48 +0600

    make indivusual leftSection jsx file for all category section

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,3 +1,5 @@
+import LeftSection from "../components/mainsection/LeftSection";
+
 const category = async ()=>{
   const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
   const data = await res.json();
@@ -8,17 +10,12 @@ const category = async ()=>{
 export default async function Home() {
 
   const categoryData = await category();
-  console.log(categoryData)
+
 
   return (
     <div className="grid grid-cols-12 gap-5 container mx-auto  ">
         <div className="col-span-3 rounded-md p-3">
-          <h2 className="font-semibold text-xl text-gray-700 pb-5">All Category</h2>
-          <ul className="text-gray-500 text-center flex flex-col gap-2 ">
-            {categoryData.map(e=><ol key={e.category_id} className="py-2 text-lg bg-slate-300">
-              {e.category_name}
-            </ol>)}
-          </ul>
+          <LeftSection category={categoryData} active={null}></LeftSection>
         </div>
         <div className="col-span-6  rounded-md p-3">
           <h2 className="font-semibold text-xl text-gray-700 pb-5">Dragon News Home</h2>
---
+++ src/app/components/mainsection/LeftSection.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import React from 'react';
+
+const LeftSection = ({ category, active }) => {
+    return (
+        <div>
+            <h2 className="font-semibold text-xl text-gray-700 pb-5">All Category</h2>
+            <ul className="text-gray-500 text-center flex flex-col gap-2 ">
+                {category.map(e => <ol key={e.category_id} className={`${active === e.category_id && "bg-slate-300 rounded-xl text-secondary font-bold"} py-2 text-lg `}>
+                    <Link className='block' href={`/category/${e.category_id}`}>
+                        {e.category_name}
+                    </Link>
+                </ol>)}
+            </ul>
+        </div>
+    );
+};
+
+export default LeftSection;
