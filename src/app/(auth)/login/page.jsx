fbbc1c5f5965dac790620dd8f0c11eb967d76924
shuvro18 <shuvrobiswas404@gmail.com>
2026-05-22 01:28:05 +0600

    login page added successfully

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,24 +1,41 @@
+'use client'
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 
-const loginPage = () => {
+
+const LoginPage = () => {
+
+
+    
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+
+    const handleLoginForm = (data) => {
+        console.log(data);
+    }
+
+
     return (
         <div className="bg-slate-100 flex items-center justify-center h-[80vh] container mx-auto">
             <div className="fieldset bg-base-200 border-base-300 w-xl border p-19 ">
                 <h2 className="text-center font-semibold text-gray-600 text-3xl">Login Your Account</h2>
                 <div className="divider py-7 border-base-300"></div>
-                <form>
-                    <fieldset className="space-y-4">
+                <form onSubmit={handleSubmit(handleLoginForm)} >
+                    <div className="space-y-4">
 
 
-                        <label className="label font-bold text-xl">Email Address</label>
-                        <input type="email" className="input w-full" placeholder="Email" />
-
-                        <label className="label font-bold text-xl">Password</label>
-                        <input type="password" className="input w-full" placeholder="Password" />
-
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend font bold text-xl text-slate-600">Email Address</legend>
+                            <input type="email" className="input w-full" placeholder="Type here Email" {...register("email field is required", { required: true })} />
+                            <p className="label">Optional</p>
+                        </fieldset>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend font bold text-xl text-slate-600">Password</legend>
+                            <input type="password" className="input w-full " placeholder="Type here password" {...register("password", { required: "password is required" })} />
+                            {errors.password && (<span className="text-red-600">{errors.password.message}</span>)}
+                        </fieldset>
                         <button className="btn btn-neutral mt-4">Login</button>
-                    </fieldset>
+                    </div>
                 </form>
                 <p className="text-center">Do not Have An Account? <Link href={"/register"} className="text-secondary" > Register </Link> </p>
             </div>
@@ -26,4 +43,4 @@ const loginPage = () => {
     );
 };
 
-export default loginPage;
+export default LoginPage;
